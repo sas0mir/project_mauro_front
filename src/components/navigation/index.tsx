ae3c4d32383601d2511a10297cbc5a49971c75ef
sas0mir <ssmirnov@i-novus.ru>
2024-12-13 23:56:04 +0100

    D-001: nav component development

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -4,8 +4,8 @@ import type { RootState } from '../../store/store'
 import { NavLink } from 'react-router'
 import styles from './navigation.module.scss'
 import classNames from 'classnames'
-import { FaTimes } from "react-icons/fa"
-import { CiMenuFries } from "react-icons/ci";
+import { FaTimes, FaAlignJustify } from "react-icons/fa"
+import { LuCross } from "react-icons/lu";
 
 function Navigation() {
 
@@ -13,14 +13,47 @@ function Navigation() {
     const theme = useSelector((state: RootState) => state.settings.theme);
     const baseUrl = import.meta.env.VITE_RELATIVE_BASE_URL || '';
 
-    const handleNavClick = () => setNavClick(!navClick);
+    const handleNavClick = () => {
+      setNavClick(!navClick);
+      if (navigator.vibrate) {
+        navigator.vibrate(50);
+      }
+    }
 
     const dropContent = (
         <>
-            <div className="navbar-drop-container bg-slate-900 transition">
-                <ul className="text-center text-xl p-20">
+            <div className={classNames(
+              styles.navbar_drop_container,
+              {
+                [styles.nav_bg_default]: theme === 'default',
+                [styles.nav_bg_earth]: theme === 'earth',
+                [styles.nav_bg_vintage]: theme === 'vintage',
+                [styles.nav_bg_cosmic]: theme === 'cosmic',
+                [styles.nav_bg_warm]: theme === 'warm',
+                [styles.nav_bg_modern]: theme === 'modern'
+              }
+              )}>
+                <ul className={classNames(
+                  styles.navbar_drop_list,
+                  {
+                    [styles.nav_border_default]: theme === 'default',
+                    [styles.nav_border_earth]: theme === 'earth',
+                    [styles.nav_border_vintage]: theme === 'vintage',
+                    [styles.nav_border_cosmic]: theme === 'cosmic',
+                    [styles.nav_border_warm]: theme === 'warm',
+                    [styles.nav_border_modern]: theme === 'modern'
+                  }
+                  )}>
+                    <NavLink
+                      to={`${baseUrl}/`}
+                      className={({ isActive }) => 
+                        isActive ? styles.navigation_link_active : styles.navigation_link
+                      }
+                    >
+                      <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
+                    </NavLink>
                     <NavLink to="/issues">
-                        <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Issues</li>
+                        
                     </NavLink>
                     <NavLink to="/logs">
                         <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Logs</li>
@@ -82,13 +115,25 @@ function Navigation() {
         </NavLink>
       </div>
       
-      <div>
-          {navClick && dropContent}
-      </div>
+      {navClick && dropContent}
 
-      <button className="navigation_btn sm:hidden transition" onClick={handleNavClick}>
-          {navClick ? <FaTimes /> : <CiMenuFries />}
+      <button className={classNames(
+      styles.navigation_btn,
+      {
+        [styles.nav_color_default]: theme === 'default',
+        [styles.nav_color_earth]: theme === 'earth',
+        [styles.nav_color_vintage]: theme === 'vintage',
+        [styles.nav_color_cosmic]: theme === 'cosmic',
+        [styles.nav_color_warm]: theme === 'warm',
+        [styles.nav_color_modern]: theme === 'modern'
+      }
+    )} onClick={handleNavClick}>
+          {navClick ? <FaTimes /> : <FaAlignJustify />}
       </button>
+      <div>
+        {/* тут аватарка юзера c выпадающим меню или иконка если нет */}
+        <LuCross />
+      </div>
     </nav>
   )
 }
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -7,7 +7,7 @@ function Homepage() {
     <div className={styles.homepage}>
       <h1 style={{fontSize: '30pt', fontWeight: 'bolder', color: 'black', margin: '1rem'}}>BENVENUTO MAURO Homepage</h1>
       <p style={{margin: '1rem', fontFamily: 'sans-serif', fontWeight: 'bold'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, rem ad necessitatibus voluptate iusto veniam quibusdam cumque qui dolor expedita incidunt fugiat sequi quaerat corporis. Eveniet dolorum necessitatibus corrupti atque!</p>
-      <img style={{width: '100%'}} src='https://diocesiluceratroia.it/wp-content/uploads/sites/2/2019/06/Sacro-Cuore-di-Gesù-1-800x437.jpg' />
+      {/* <img style={{width: '100%'}} src='https://diocesiluceratroia.it/wp-content/uploads/sites/2/2019/06/Sacro-Cuore-di-Gesù-1-800x437.jpg' /> */}
     </div>
   )
 }
