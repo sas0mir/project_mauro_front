b60e74b1d94dc4f8de436befe135ce996a2a906a
sas0mir <sas_mir@icloud.com>
2025-06-30 23:49:59 +0200

    D_014: docker, layout and pnpm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,28 @@
 import '@/styles/globals.css'
 import { Limelight, Montserrat } from 'next/font/google'
 import type { AppProps } from 'next/app'
-import { store } from '../store/store'
-import Navigation from '../components/navigation'
-import type { RootState } from '../store/store'
+import { useReportWebVitals } from 'next/web-vitals'
 
 const limelight = Limelight({ subsets: ['latin'], weight: '400', variable: '--font-limelight' })
 const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' })
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  //todo analytics
+  useReportWebVitals((metric) => {
+    switch (metric.name) {
+      case 'FCP': {
+        // handle FCP results
+      }
+      case 'LCP': {
+        // handle LCP results
+      }
+      case 'FID': {
+        // handle LCP results
+      }
+      // ...
+    }
+  })
 
   return (
     <main className={`app_container ${montserrat.variable} ${limelight.variable}`}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,21 @@
 import Homepage from './homepage'
-export default Homepage
+import LoginPage from './login'
+import { useEffect, useState } from 'react'
+import { useAuth } from '@/composables/useAuth' //todo
+
+export default function IndexPage() {
+  const { status } = useAuth()
+  const [loading, setLoading] = useState(true)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  useEffect(() => {
+    //todo
+    const token = localStorage.getItem('token')
+    setIsAuthenticated(!!token)
+    setLoading(false)
+  }, [])
+
+  if (loading) return <div>Загрузка...</div>
+
+  return isAuthenticated ? <Homepage /> : <LoginPage />
+}
